6e22155a8ab39b971d37d3f2e201bb1b7fe58710
halboi <halparke22@gmail.com>
2026-04-01 13:52:40 +0100

    Add Supabase configuration file
    Added configuration for Supabase with project URL and anon key.

---
+++ supabase config.js
@@ -0,0 +1,9 @@
+// ===================================================
+//  SUPABASE CONFIG
+//  Fill in your project URL and anon key below.
+//  You get these from: supabase.com → your project
+//  → Settings → API
+// ===================================================
+
+const SUPABASE_URL  = 'https://uuxiqqdosoqrstszlyzc.supabase.co;   // e.g. https://xyzabc.supabase.co
+const SUPABASE_ANON = 'b_publishable_flXNKSmc9VkOOIe2--sDUw_YqWjfD3Z'; // long string starting with "eyJ..."
